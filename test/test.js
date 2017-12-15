5c9fe747879ee81a1cdbee634b9fbae5d32f99da
Scott González <scott.gonzalez@gmail.com>
2017-12-15 17:08:14 -0500

    Switch to standard

--- test/test.js
+++ test/test.js
@@ -1,83 +1,84 @@
-var fs = require( "fs" );
-var rimraf = require( "rimraf" );
-var path = __dirname + "/repo";
-var Repo = require( "../git-tools" );
-var testRepo = new Repo( path );
+var fs = require('fs')
+var path = require('path')
+var rimraf = require('rimraf')
+var repoPath = path.join(__dirname, 'repo')
+var Repo = require('../git-tools')
+var testRepo = new Repo(repoPath)
 
-function createRepo( callback ) {
-	rimraf.sync( path );
-	fs.mkdirSync( path );
-	testRepo.exec( "init", function( error ) {
-		if ( error ) {
-			throw error;
-		}
+function createRepo (callback) {
+  rimraf.sync(repoPath)
+  fs.mkdirSync(repoPath)
+  testRepo.exec('init', function (error) {
+    if (error) {
+      throw error
+    }
 
-		testRepo.exec( "commit", "--allow-empty",
-			"--author=John Doe <john.doe@example.com>", "-m first commit",
-			function( error ) {
-				if ( error ) {
-					throw error;
-				}
+    testRepo.exec(
+      'commit', '--allow-empty', '--author=John Doe <john.doe@example.com>', '-m first commit',
+    function (error) {
+      if (error) {
+        throw error
+      }
 
-				testRepo.exec( "tag", "-a", "v0.1", "-m", "tag", callback );
-			});
-	});
+      testRepo.exec('tag', '-a', 'v0.1', '-m', 'tag', callback)
+    })
+  })
 }
 
 exports.authors = {
-	"one commit": function( test ) {
-		createRepo(function( error ) {
-			if ( error ) {
-				throw error;
-			}
+  'one commit': function (test) {
+    createRepo(function (error) {
+      if (error) {
+        throw error
+      }
 
-			testRepo.authors(function( error, authors ) {
-				test.ifError( error );
+      testRepo.authors(function (error, authors) {
+        test.ifError(error)
 
-				test.deepEqual( authors, [
-					{
-						email: "john.doe@example.com",
-						name: "John Doe",
-						commits: 1,
-						commitsPercent: 100
-					}
-				], "one author" );
-				test.done();
-			});
-		});
-	}
-};
+        test.deepEqual(authors, [
+          {
+            email: 'john.doe@example.com',
+            name: 'John Doe',
+            commits: 1,
+            commitsPercent: 100
+          }
+        ], 'one author')
+        test.done()
+      })
+    })
+  }
+}
 
 exports.describe = {
-	"default description": function( test ) {
-		createRepo(function( error ) {
-			if ( error ) {
-				throw error;
-			}
+  'default description': function (test) {
+    createRepo(function (error) {
+      if (error) {
+        throw error
+      }
 
-			testRepo.describe(function( error, description ) {
-				test.ifError( error );
+      testRepo.describe(function (error, description) {
+        test.ifError(error)
 
-				test.equal( description, "v0.1", "a tag");
-				test.done();
-			});
-		});
-	},
+        test.equal(description, 'v0.1', 'a tag')
+        test.done()
+      })
+    })
+  },
 
-	"long description": function( test ) {
-		createRepo(function( error ) {
-			if ( error ) {
-				throw error;
-			}
+  'long description': function (test) {
+    createRepo(function (error) {
+      if (error) {
+        throw error
+      }
 
-			testRepo.describe( {
-				long: true
-			}, function( error, description ) {
-				test.ifError( error );
+      testRepo.describe({
+        long: true
+      }, function (error, description) {
+        test.ifError(error)
 
-				test.ok( /v0\.1-0-g.{7}/.test( description ), "a long tag");
-				test.done();
-			});
-		});
-	}
-};
+        test.ok(/v0\.1-0-g.{7}/.test(description), 'a long tag')
+        test.done()
+      })
+    })
+  }
+}
